716e030c04b56e5938a517a2ec2c9ed011707fce
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-09-13 17:50:40 +0300

    Lisäykset.

--- JavaScriptMVCTesti/JavaScriptMVCTesti/Scripts/Testi1.js
+++ JavaScriptMVCTesti/JavaScriptMVCTesti/Scripts/Testi1.js
@@ -1,1 +1,5 @@
 ﻿var globaaliArvo = 987;
+
+function näytäArvo1() {
+    alert("Tiedostosta Testi1.js, arvo = " + globaaliArvo);
+}
---
+++ JavaScriptMVCTesti/JavaScriptMVCTesti/Scripts/Testi2.js
@@ -0,0 +1,6 @@
+﻿//var globaaliArvo = 987;
+
+
+function näytäArvo2() {
+    alert("Tiedostosta Testi2.js, arvo = " + globaaliArvo);
+}
